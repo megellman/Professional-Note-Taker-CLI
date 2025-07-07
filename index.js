1c7e5ed9de1edc2bd7eea1ea473f845224cb4674
Megan Ellman <meganlellman@gmail.com>
2025-07-06 21:44:37 -0700

    created inquirer prompt

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            name: 'action',
+            type: 'list',
+            message: 'Please select an action:',
+            
+        }
+    ])
