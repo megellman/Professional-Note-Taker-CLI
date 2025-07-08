25d7645b18539af00d2a4dcd46e03200c5aafd6e
Megan Ellman <meganlellman@gmail.com>
2025-07-07 21:13:09 -0700

    view notes

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ inquirer
             name: 'action',
             type: 'list',
             message: 'Please select an action:',
-            
+            choices: ['create a new note', 'view/edit a note', 'delete a note'],
+        },{
+            name: 'create-title',
+            type: 'input',
+            message: 'What is this note called?',
+            when: (res) => res.action === 'create a new note',
+        },{
+            name: 'create-content',
+            type: 'input',
+            message: 'Enter your note',
+            when: (res) => res.action === 'create a new note',
+        },{
+            name: 'select-note',
+            type: 'list',
+            message: 'select a note',
+            choices: getNotes(),
         }
     ])
+    .then((res) => {
+        console.log(res)
+    })
+
+    function getNotes(){
+        return fs.readdirSync('./notes-folder');
+    }
+
+    
